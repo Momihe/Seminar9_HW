d686983ad56ada841a784f5ae3da3e59a5ec1026
Momihe <momihe@mail.ru>
2023-01-12 17:48:48 +0500

    HW9

---
+++ Ex64/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 64: Задайте значение M и N. Напишите программу, которая выведет 
+все натуральные числа в промежутке от N до M. Выполнить с помощью рекурсии.
+
+M = 1; N = 5. -> "5,4,3,2,1"
+
+M = 4; N = 8. -> "8, 7, 6, 5, 4"
+*/
+
+
+int Number(int M, int N, int[] mas)
+{
+    if (M > N) return M;
+    else return mas[N-M] = Number(M + 1, N, mas) - 1;
+}
+
+Console.Clear();
+Console.Write("Введите М = ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] mas = new int[N - M + 1];
+Number(M, N, mas);
+Console.WriteLine($"{string.Join(", ", mas)}");
+
+
+
+
---
+++ Ex66/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int sum(int M, int N)
+{
+    if (M == N) return N;
+    else return N + sum(M, N -1); 
+}
+
+
+Console.Clear();
+Console.Write("Введите М = ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(sum(M, N));
